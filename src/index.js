a2a9ebe56c208b2f56b3d313ecc5321abc4309dc
Rohit Surya <33322975+RohitSurya@users.noreply.github.com>
2023-09-21 23:04:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.js';
 import './i18n/config';
 import reportWebVitals from './reportWebVitals';
 
